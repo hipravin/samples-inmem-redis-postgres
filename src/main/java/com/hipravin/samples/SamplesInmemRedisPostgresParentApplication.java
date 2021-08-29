414c5ae89033f45be32212626f81f57470fd2ad2
Alex <alexander.vmk@gmail.com>
2021-08-29 13:44:22 +0300

    empty spring boot project

---
+++ src/main/java/com/hipravin/samples/SamplesInmemRedisPostgresParentApplication.java
@@ -0,0 +1,13 @@
+package com.hipravin.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplesInmemRedisPostgresParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamplesInmemRedisPostgresParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hipravin/samples/SamplesInmemRedisPostgresParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hipravin.samples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplesInmemRedisPostgresParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
